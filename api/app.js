ed5f713003223c85e941b718bf8398581e6ec478
serhat <serhat.karabiyik@outlook.fr>
2024-03-01 14:00:01 +0100

    Add firebase and configuration

---
+++ api/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGQeAy84UeQZ0nDqMv978PJ5HQsx1pgwc",
+  authDomain: "react-native-tp.firebaseapp.com",
+  projectId: "react-native-tp",
+  storageBucket: "react-native-tp.appspot.com",
+  messagingSenderId: "464501116118",
+  appId: "1:464501116118:web:34d653e97410415dddcd77",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
